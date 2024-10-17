7de5bd770db9510b7b81cc5b65749bfd1f096082
Tomaz Ferreira De Lima <toferrei@c2r2s1.42lisboa.com>
2024-10-17 14:17:39 +0100

    correction for intmax intmin being last argument

--- Sources/verifs.c
+++ Sources/verifs.c
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:03:44 by toferrei          #+#    #+#             */
-/*   Updated: 2024/10/15 10:14:39 by toferrei         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:14:26 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int	is_list_valid(t_data *data, long *lst)
 		m = n + 1;
 		while (m < data->size)
 		{
-			if (lst[n] == lst[m] | lst[n] > 2147483647 | lst[n] < -2147483648)
+			if (lst[n] == lst[m] | lst[m] > 2147483647 | lst[m] < -2147483648)
 				return (0);
 			m++;
 		}
